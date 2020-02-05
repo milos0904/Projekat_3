83e3681a4074215103834deb4dab1cf2d49ba5d9
milos0904 <st.milosposlovni@gmail.com>
2020-02-05 22:47:43 +0100

    -

--- BetPharm/BetPharm/Login.Designer.cs
+++ BetPharm/BetPharm/Login.Designer.cs
@@ -88,6 +88,7 @@
             this.Controls.Add(this.txtUsername);
             this.Controls.Add(this.label1);
             this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
             this.ResumeLayout(false);
             this.PerformLayout();
