57c15abda2bd50c7c99d5f6755cdef0b3806b51c
milos0904 <st.milosposlovni@gmail.com>
2020-02-05 22:44:31 +0100

    odavite se promenjeno u odjavite se

--- BetPharm/BetPharm/Form1.Designer.cs
+++ BetPharm/BetPharm/Form1.Designer.cs
@@ -94,7 +94,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(124, 23);
             this.button1.TabIndex = 4;
-            this.button1.Text = "Odavite se";
+            this.button1.Text = "Odjavite se";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
